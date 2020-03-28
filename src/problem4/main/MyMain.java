2308412b622a5be83f7fdbb2eaab0506a27f6c94
kb-s <kanhaiya.bhatt_cs18@gla.ac.in>
2020-03-28 16:29:31 +0530

    create a main class

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,5 +7,24 @@
 package problem4.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        System.out.println("How many elements do you want in the queue?");
+        final Scanner scanner = new Scanner(System.in);
+        int numberOfElements = scanner.nextInt();
+        for (int i = 0; i < numberOfElements; i++) {
+            bst.add(scanner.nextInt());
+
+        }
+        MyQueue queue = new MyQueue();
+        queue.enQueue(bst.root);
+        queue.printQueue();
+    }
 }
