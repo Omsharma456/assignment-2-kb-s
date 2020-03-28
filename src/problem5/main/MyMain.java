ef86b8d6789ff34374fcbedeebad84fb1efc5d23
kb-s <kanhaiya.bhatt_cs18@gla.ac.in>
2020-03-28 16:23:57 +0530

    create a main class for this problem

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,5 +6,22 @@
  */
 package problem5.main;
 //executable class
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
+
+import java.util.ArrayList;
+
 public class MyMain {
+    public static void main(String[] args) {
+        ArrayList<Student> studentList = new ArrayList<>();
+        studentList.add(new Student(1, "kb", 0, 7));
+        studentList.add(new Student(2, "amy", , 1));
+        studentList.add(new Student(3, "bb", 7, 3));
+        studentList.add(new Student(4, "thor", 1, 5));
+        studentList.add(new Student(5, "Dr_nf", 5, 2));
+        MyCircularQueue queue = new MyCircularQueue();
+        queue.enQueue(studentList);
+        queue.printQueue();
+    }
 }
